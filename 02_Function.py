6ae56f2b8de3c24b7cfd8fda52dede32dbaa653d
Hemant Kumar Singh <ksingh1617@gmail.com>
2024-12-24 22:02:11 +0530

    Add files via upload

---
+++ 02_Function.py
@@ -0,0 +1,20 @@
+import random
+
+# def with parameters
+
+# def hello(name):
+#     print("Hi my name is " + name)
+# hello('Alex')
+#
+# def getAnswer(answerNumber):
+#     if answerNumber == 1:
+#         return "Omg"
+#     elif answerNumber == 2:
+#         return "Oops"
+#     elif answerNumber == 3:
+#         return "Wtf"
+#
+# r = random.randint(1,3)
+# fortune = getAnswer(r)
+# print(fortune)
+
