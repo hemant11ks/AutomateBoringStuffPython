b462612e35a2e37c80745a0d4d75db3cda134a14
Hemant Kumar Singh <ksingh1617@gmail.com>
2024-12-28 22:44:28 +0530

    Add files via upload

---
+++ 03_Lists.py
@@ -0,0 +1,40 @@
+# spam = [['cat', 'bat'], [10,20,30]]
+# print(spam[0])
+# print(spam[0][0])
+# print(spam[0][1])
+# print(spam[1][2])
+
+# ['cat', 'bat']
+# cat
+# bat
+# 30
+# It will give error due to out of range
+# print(spam[0][4])
+
+# Iterating from end of the list
+# ls = [1,2,3,4]
+# print(ls[-1]) # 4
+
+# Getting sublist and slices
+
+# spam[2] : is a list with an index
+# spam[1:4] : is a list with a slice (two integers)
+
+# In slice first int is where the slice will start and the second integer is where the slice will end
+
+# spam = ['a', 'b', 'c', 'd']
+# print(spam[0:2])
+# print(spam[0:-2])
+#
+# ['a', 'b']
+# ['a', 'b']
+
+# As a shortcut we can also leave one param like:
+# spam = ['cat', 'dog', 'bat', 'elephant']
+# print(spam[:2])
+# # ['cat', 'dog']
+# print(spam[1:])
+# # ['dog', 'bat', 'elephant']
+
+
+
