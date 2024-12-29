7bd694e5aa25e45b4ef55ec8cf11b42e795304d4
Hemant Kumar Singh <ksingh1617@gmail.com>
2024-12-29 23:03:40 +0530

    Add files via upload

---
+++ 04_Dictionary.py
@@ -0,0 +1,5 @@
+# Dictionary is a mutable collection of many values
+
+# Indexes for dictionaries can use many different data types, not just integers.
+# Indexes for dictionaries are called keys, and a key with its associated value is called a key-value pair.
+
